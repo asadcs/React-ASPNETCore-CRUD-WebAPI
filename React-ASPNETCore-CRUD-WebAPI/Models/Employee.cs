b4330490f801bea3474e6abf96f9e5097866f1b0
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-03-13 15:29:33 -0800

    Model Classes Added

---
+++ React-ASPNETCore-CRUD-WebAPI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace React_ASPNETCore_CRUD_WebAPI.Models
+{
+    public class Employee
+    {
+        public long EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string DepartmentName { get; set; }
+        public string MailID { get; set; }
+
+        public DateTime? DOJ { get; set; }
+    }
+}
