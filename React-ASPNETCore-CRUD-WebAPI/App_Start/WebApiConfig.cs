2f1168fb6d062a8f3111acc099f0aaaf8fcc1c8e
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-03-13 13:53:04 -0800

    Force Web API to send JSON response

--- React-ASPNETCore-CRUD-WebAPI/App_Start/WebApiConfig.cs
+++ React-ASPNETCore-CRUD-WebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace React_ASPNETCore_CRUD_WebAPI
 {
@@ -19,6 +21,9 @@ namespace React_ASPNETCore_CRUD_WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //Force Web API to send JSON response
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
